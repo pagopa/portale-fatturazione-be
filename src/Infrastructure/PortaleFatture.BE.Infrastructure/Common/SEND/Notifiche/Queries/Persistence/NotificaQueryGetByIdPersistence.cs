efcc06bed1b4612355c73f3a4b3bf8e292601479
Mauro Ghiani <mauro.ghiani@porini.it>
2024-12-06 11:13:25 +0000

    Index event_id notifiche
    Related work items: #524

--- src/Infrastructure/PortaleFatture.BE.Infrastructure/Common/SEND/Notifiche/Queries/Persistence/NotificaQueryGetByIdPersistence.cs
+++ src/Infrastructure/PortaleFatture.BE.Infrastructure/Common/SEND/Notifiche/Queries/Persistence/NotificaQueryGetByIdPersistence.cs
@@ -1,7 +1,6 @@
 ﻿using System.Data;
 using PortaleFatture.BE.Core.Entities.SEND.Notifiche;
 using PortaleFatture.BE.Infrastructure.Common.Persistence;
-using PortaleFatture.BE.Infrastructure.Common.SEND.Notifiche.Queries;
 using PortaleFatture.BE.Infrastructure.Common.SEND.Notifiche.Queries.Persistence.Builder;
 
 namespace PortaleFatture.BE.Infrastructure.Common.SEND.Notifiche.Queries.Persistence;
@@ -12,7 +11,7 @@ public class NotificaQueryGetByIdPersistence(NotificaQueryGetById command) : Dap
     private static readonly string _sqlAll = NotificaSQLBuilder.SelectAll();
     public async Task<Notifica?> Execute(IDbConnection? connection, string schema, IDbTransaction? transaction, CancellationToken cancellationToken = default)
     {
-        var sql = _sqlAll + " WHERE event_id=@IdNotifica ";
+        var sql = _sqlAll + " WHERE n.event_id=@IdNotifica ";
         try
         {
             var notifica = await ((IDatabase)this).SingleAsync<Notifica>(
