57d9f23a5c1fe322afc25555f4b23ac518d16431
Mauro Ghiani <mauro.ghiani@porini.it>
2025-06-05 11:12:13 +0000

    fix: 640
    Merged PR 203: fix maggio INPS
    
    Related work items: #640

--- src/Presentation/PortaleFatture.BE.Api/Modules/SEND/Notifiche/NotificaModule.cs
+++ src/Presentation/PortaleFatture.BE.Api/Modules/SEND/Notifiche/NotificaModule.cs
@@ -548,7 +548,7 @@ public partial class NotificaModule
     {
         var authInfo = context.GetAuthInfo();
         var tempAnno = 2025;
-        int[] tempMese = [1, 2, 3, 4];
+        int[] tempMese = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
         var tempIdEnte = "53b40136-65f2-424b-acfb-7fae17e35c60";
         var tempName = "inps";
 
