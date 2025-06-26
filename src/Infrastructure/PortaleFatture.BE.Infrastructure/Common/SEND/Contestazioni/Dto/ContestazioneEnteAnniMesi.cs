f4cff5920525943dd1e221b62af7008c5512ed94
Mauro Ghiani <mauro.ghiani@porini.it>
2025-06-26 10:42:21 +0000

    fix: 643
    Merged PR 207: Contestazioni azione anni e mesi Ente
    
    Related work items: #643

---
+++ src/Infrastructure/PortaleFatture.BE.Infrastructure/Common/SEND/Contestazioni/Dto/ContestazioneEnteAnniMesi.cs
@@ -0,0 +1,7 @@
+﻿namespace PortaleFatture.BE.Infrastructure.Common.SEND.Contestazioni.Dto;
+
+public class ContestazioneEnteAnniMesi
+{
+    public string? AnnoContestazione { get; set; }
+    public string? MeseContestazione { get; set; }
+} 
