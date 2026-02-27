9b1b6ad054d72af773f4cb77930260a5b0bc015b
Cacciamani Luca <luca.cacciamani@dgsspa.com>
2026-02-27 17:52:46 +0000

    fix: 746
    Merged PR 279: Fix merged main problem
    
    Related work items: #729, #746

--- src/Infrastructure/PortaleFatture.BE.Infrastructure/Common/SEND/Fatture/QueryHandlers/FattureEliminateExcelHandler.cs
+++ src/Infrastructure/PortaleFatture.BE.Infrastructure/Common/SEND/Fatture/QueryHandlers/FattureEliminateExcelHandler.cs
@@ -15,13 +15,13 @@ public class FattureEliminateExcelHandler(
     IFattureDbContextFactory factory,
     IStringLocalizer<Localization> localizer,
     ILogger<FattureEliminateExcelHandler> logger)
-    : IRequestHandler<FattureEliminateExcelQuery, FattureDocContabiliEliminateDtoList>
+    : IRequestHandler<FattureEliminateExcelQuery, IEnumerable<FattureEliminateExcelDto>>
 {
     private readonly IFattureDbContextFactory _factory = factory;
     private readonly ILogger<FattureEliminateExcelHandler> _logger = logger;
     private readonly IStringLocalizer<Localization> _localizer = localizer;
 
-    public async Task<FattureDocContabiliEliminateDtoList> Handle(FattureEliminateExcelQuery request, CancellationToken cancellationToken)
+    public async Task<IEnumerable<FattureEliminateExcelDto>> Handle(FattureEliminateExcelQuery request, CancellationToken cancellationToken)
     {
         var query = new FattureEliminateQuery(request.AuthenticationInfo)
         {
@@ -34,6 +34,24 @@ public class FattureEliminateExcelHandler(
         using var rs = await _factory.Create(cancellationToken: cancellationToken);
         var rawResult = await rs.Query(new FattureEliminateQueryPersistence(query), cancellationToken);
 
-        return rawResult.ToEliminateDto();
+        var dtoList = rawResult.ToEliminateDto();
+
+        if (dtoList.Dettagli == null)
+            return [];
+        return dtoList.Dettagli.Select(x => new FattureEliminateExcelDto
+        {
+            IstitutioId = x.IstitutioId,
+            RagioneSociale = x.RagioneSociale,
+            IdContratto = x.IdContratto,
+            Prodotto = x.Prodotto,
+            TipologiaFattura = x.TipologiaFattura,
+            Progressivo = x.Progressivo,
+            DataFattura = x.DataFattura,
+            TotaleFattura = x.TotaleFattura,
+            Stato = x.Stato,
+            CausaleFattura = x.CausaleFattura,
+            TipoDocumento = x.TipoDocumento,
+            Divisa = x.Divisa
+        });
     }
 }
