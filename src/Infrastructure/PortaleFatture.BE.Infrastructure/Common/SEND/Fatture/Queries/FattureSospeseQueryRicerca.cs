aac64474a78c310c4ccf4fd3b3fe8f558ad49499
Cacciamani Luca <luca.cacciamani@dgsspa.com>
2026-03-04 10:20:16 +0000

    feat: 749
    Merged PR 281: Aggiunta endpoint e query per ricerca fatture sospese #749
    
    Related work items: #749

---
+++ src/Infrastructure/PortaleFatture.BE.Infrastructure/Common/SEND/Fatture/Queries/FattureSospeseQueryRicerca.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using PortaleFatture.BE.Core.Auth;
+using PortaleFatture.BE.Infrastructure.Common.SEND.Fatture.Dto;
+
+namespace PortaleFatture.BE.Infrastructure.Common.SEND.Fatture.Queries;
+
+public class FattureSospeseQueryRicerca(IAuthenticationInfo authenticationInfo) : IRequest<FattureListaDto?>
+{
+    public IAuthenticationInfo AuthenticationInfo { get; internal set; } = authenticationInfo;
+    public string[]? IdEnti { get; set; }
+    public string[]? TipologiaFattura { get; set; }
+    public int? Anno { get; set; }
+    public int? Mese { get; set; }
+    public bool Cancellata { get; set; }
+    public int? FkIdTipoContratto { get; set; }
+    public int? FatturaInviata { get; set; }
+}
