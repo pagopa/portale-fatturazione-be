52aecfc01d9cb04f3b12e334462a041a9f8fa77c
Cacciamani Luca <luca.cacciamani@dgsspa.com>
2026-03-17 14:54:05 +0000

    fix: 764
    Merged PR 298: Endpoint dettaglio Documento Contabile sospeso (Amministratore)
    
    Related work items: #764

---
+++ src/Presentation/PortaleFatture.BE.Api/Modules/SEND/Fatture/Payload/Request/FattureDocContabileEnteAdminRequest.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using PortaleFatture.BE.Core.Auth;
+using PortaleFatture.BE.Core.Extensions;
+using PortaleFatture.BE.Infrastructure.Common.SEND.Fatture.Dto;
+
+namespace PortaleFatture.BE.Api.Modules.SEND.Fatture.Payload.Request
+{
+    public class FattureDocContabileEnteAdminRequest() : FattureDocContabileEnteRequest
+    {
+        public string? IdEnte { get; set; }
+    }
+}
