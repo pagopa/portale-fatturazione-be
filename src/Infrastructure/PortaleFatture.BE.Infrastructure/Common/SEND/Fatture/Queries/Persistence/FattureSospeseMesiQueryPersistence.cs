c8886622322f291d7d8e67bc7cbf1d418a6e67b0
Cacciamani Luca <luca.cacciamani@dgsspa.com>
2026-03-30 16:02:17 +0000

    fix: 794
    Merged PR 307: Sistemata query anni e mesi documenti Sospesi
    
    Related work items: #794

--- src/Infrastructure/PortaleFatture.BE.Infrastructure/Common/SEND/Fatture/Queries/Persistence/FattureSospeseMesiQueryPersistence.cs
+++ src/Infrastructure/PortaleFatture.BE.Infrastructure/Common/SEND/Fatture/Queries/Persistence/FattureSospeseMesiQueryPersistence.cs
@@ -1,5 +1,4 @@
 using System.Data;
-using System.Dynamic;
 using PortaleFatture.BE.Infrastructure.Common.Persistence;
 using PortaleFatture.BE.Infrastructure.Common.SEND.Fatture.Queries.Persistence.Builder;
 
@@ -12,15 +11,12 @@ public class FattureSospeseMesiQueryPersistence(FattureSospeseMesiQuery command)
     private static readonly string _orderBy = FattureQueryRicercaBuilder.OrderByMonth();
     public async Task<IEnumerable<string>?> Execute(IDbConnection? connection, string schema, IDbTransaction? transaction, CancellationToken cancellationToken = default)
     {
-        dynamic parameters = new ExpandoObject();
-        var where = string.Empty;
-        if (!string.IsNullOrEmpty(_command.Anno))
+        var parameters = new
         {
-            where += " AND annoriferimento=@anno ";
-            parameters.Anno = _command.Anno;
-        }
+            _command.Anno
+        };
 
         return await ((IDatabase)this).SelectAsync<string>(
-            connection!, _sql + where + _orderBy, parameters, transaction);
+            connection!, _sql + _orderBy, parameters, transaction);
     }
 }
