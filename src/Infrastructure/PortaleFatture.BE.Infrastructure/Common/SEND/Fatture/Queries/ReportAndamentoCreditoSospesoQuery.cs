222ced2ab7dcc953580b9f53b42696a2f58bfd12
Cacciamani Luca <luca.cacciamani@dgsspa.com>
2026-06-11 18:12:47 +0200

    Aggiunto supporto query report credito sospeso fatture
    Introdotti tre nuovi file: builder per query SQL parametrica, classe di persistenza per esecuzione tramite Dapper e query con parametri e autenticazione. Migliorata la separazione delle responsabilità e la riusabilità nella gestione delle query per il report sull’andamento del credito sospeso.

---
+++ src/Infrastructure/PortaleFatture.BE.Infrastructure/Common/SEND/Fatture/Queries/ReportAndamentoCreditoSospesoQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using PortaleFatture.BE.Core.Auth;
+using PortaleFatture.BE.Infrastructure.Common.SEND.Fatture.Dto;
+
+namespace PortaleFatture.BE.Infrastructure.Common.SEND.Fatture.Queries;
+
+
+public class ReportAndamentoCreditoSospesoQuery(IAuthenticationInfo authenticationInfo)
+    : IRequest<IEnumerable<ReportAndamentoCreditoSospesoDto>?>
+{
+    public IAuthenticationInfo AuthenticationInfo { get; internal set; } = authenticationInfo;
+
+    public int? Anno { get; set; }
+    public int? Mese { get; set; }
+    public string[]? TipologiaFattura { get; set; }
+}
