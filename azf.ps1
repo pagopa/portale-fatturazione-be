ec1357637084cd208123f9a21bb43f5e6a92c428
Cacciamani Luca <luca.cacciamani@dgsspa.com>
2026-06-18 14:56:07 +0200

    chore: remove hardcoded webhook secret

--- azf.ps1
+++ azf.ps1
@@ -1,1 +1,15 @@
-$uri = "https://fat-u-integration-func-staging.scm.azurewebsites.net/api/registry/webhook" $auth = "fat-u-integration-func__staging:[REDACTED]" $headers = @{ "Content-Type" = "application/json" }  $response = Invoke-RestMethod -Uri $uri -Method Post -Headers $headers -Body '{}' -Credential (New-Object System.Management.Automation.PSCredential($auth.Split(":")[0], (ConvertTo-SecureString $auth.Split(":")[1] -AsPlainText -Force)))  $response
+$uri = "https://fat-u-integration-func-staging.scm.azurewebsites.net/api/registry/webhook"
+$authUser = "fat-u-integration-func__staging"
+$authPassword = $env:AZF_WEBHOOK_PASSWORD
+$headers = @{ "Content-Type" = "application/json" }
+
+if (-not $authPassword) {
+	throw "Set the AZF_WEBHOOK_PASSWORD environment variable before running this script."
+}
+
+$securePassword = ConvertTo-SecureString $authPassword -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential($authUser, $securePassword)
+
+$response = Invoke-RestMethod -Uri $uri -Method Post -Headers $headers -Body '{}' -Credential $credential
+
+$response
