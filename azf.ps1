28aaff24a74bb6612ec9d69b17a5e0198319a365
Cacciamani Luca <luca.cacciamani@dgsspa.com>
2026-06-19 09:45:40 +0000

    Updated azf.ps1

--- azf.ps1
+++ azf.ps1
@@ -1,7 +1,16 @@
 $uri = "https://fat-u-integration-func-staging.scm.azurewebsites.net/api/registry/webhook"
-$auth = "fat-u-integration-func__staging:l8DctrfAt0g48crW0QAGwucdXMtAuXdhd90c7HTgssh8xcucSoyg0AGqRH9Y"
+$authUser = "fat-u-integration-func__staging"
+$authPassword = $env:AZF_WEBHOOK_PASSWORD
 $headers = @{ "Content-Type" = "application/json" }
 
-$response = Invoke-RestMethod -Uri $uri -Method Post -Headers $headers -Body '{}' -Credential (New-Object System.Management.Automation.PSCredential($auth.Split(":")[0], (ConvertTo-SecureString $auth.Split(":")[1] -AsPlainText -Force)))
+if (-not $authPassword) {
+	throw "Set the AZF_WEBHOOK_PASSWORD environment variable before running this script."
+}
+
+$securePassword = ConvertTo-SecureString $authPassword -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential($authUser, $securePassword)
+
+$response = Invoke-RestMethod -Uri $uri -Method Post -Headers $headers -Body '{}' -Credential $credential
 
 $response
+
